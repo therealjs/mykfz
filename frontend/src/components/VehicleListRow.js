18a4de5cb88259e6e9a88716a434a949d2518d29
Janik Schnellbach <janik.schnellbach@gmail.com>
2021-06-01 19:30:10 +0200

    update dashboard view and adjust footer

--- frontend/src/components/VehicleListRow.js
+++ frontend/src/components/VehicleListRow.js
@@ -22,14 +22,10 @@ export class VehicleListRow extends React.Component {
           </Link>
         </TableColumn>
         <TableColumn>
-          <SimpleLink to={`/show/${this.props.vehicle._id}`}>
             {this.props.vehicle.vin}
-          </SimpleLink>
         </TableColumn>
         <TableColumn>
-          <SimpleLink to={`/show/${this.props.vehicle._id}`}>
             {this.props.vehicle.licensePlate}
-          </SimpleLink>
         </TableColumn>
         <TableColumn>
             {this.props.vehicle.state}
@@ -45,32 +41,6 @@ export class VehicleListRow extends React.Component {
           <TableColumn>
             <Link to={"/login"}>
               <FontIcon>mode_edit</FontIcon>
-            </Link>
-          </TableColumn>
-        )}
-        {UserService.isAuthenticated() ? (
-          <TableColumn>
-            <Link to={`/vehicle/${this.props.vehicle._id}`}>
-              <FontIcon>add</FontIcon>
-            </Link>
-          </TableColumn>
-        ) : (
-          <TableColumn>
-            <Link to={"/login"}>
-              <FontIcon>add</FontIcon>
-            </Link>
-          </TableColumn>
-        )}
-        {UserService.isAuthenticated() ? (
-          <TableColumn>
-            <Link to={`/edit/${this.props.vehicle._id}`}>
-              <FontIcon>remove</FontIcon>
-            </Link>
-          </TableColumn>
-        ) : (
-          <TableColumn>
-            <Link to={"/login"}>
-              <FontIcon>remove</FontIcon>
             </Link>
           </TableColumn>
         )}
