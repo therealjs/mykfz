17d2efd310cd65a367555140a86bb0a040d2d054
Seelbach, Jan <jan.seelbach@accso.de>
2021-06-07 15:43:19 +0200

    add district model and controller

---
+++ backend/src/models/district.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+// Define the District schema
+const DistrictSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    kfz: {
+        type: String,
+        required: true
+    },
+    picture: {
+        type: String,
+        required: true
+    }
+});
+
+DistrictSchema.set('versionKey', false);
+
+// Export the District model
+module.exports = mongoose.model('District', DistrictSchema);
