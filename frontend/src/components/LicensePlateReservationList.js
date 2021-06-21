3369fe4787cf67f69456f8de26ec11a281f86c35
Seelbach, Jan <jan.seelbach@accso.de>
2021-06-21 17:01:37 +0200

    [25] Process Detail View II - add collapsable table with processes - change date formats to UTC - add keys to remove browser warnings

--- frontend/src/components/LicensePlateReservationList.js
+++ frontend/src/components/LicensePlateReservationList.js
@@ -7,60 +7,18 @@ import { Button, Card, Grid, IconButton } from '@material-ui/core';
 import LicensePlate from './LicensePlate';
 import AddIcon from '@material-ui/icons/Add';
 
-import LicensePlateService from '../services/LicensePlateService';
-
 const makeLogos = require('../../resources/carLogos');
 
-// const licensePlateReservations = [
-//     {
-//         "plate": {
-//             "areaCode": "M",
-//             "letters": "EF",
-//             "digits": "123"
-//         },
-//         "expiryDate": "2021-09-09"
-//     },
-//     {
-//         "plate": {
-//             "areaCode": "MIL",
-//             "letters": "EF",
-//             "digits": "123"
-//         },
-//         "expiryDate": "2021-20-09"
-//     }
-// ]
-
-// const async = (id) => {
-//     try {
-
-//     } catch (err) {
-
-//     }
-// };
-
 export const LicensePlateReservationList = ({ licensePlateReservations }) => {
-    // componentWillMount(props) {
-    //     (async () => {
-    //         try {
-    //             let licensePlate = await LicensePlateService.getLicensePlate(
-    //                 this.props.vehicle.licensePlate
-    //             );
-    //             this.setState({
-    //                 licensePlate: licensePlate.areaCode + " - " +  licensePlate.letters + " " + licensePlate.digits
-    //             });
-    //         } catch (err) {
-    //             console.error(err);
-    //         }
-    //     })();
-    // }
 
     return (
         <Grid item xs={12} sm={6} md={12}>
             <Card style={{ height: '200px' }}>
                 {licensePlateReservations.length > 0 ? (
                     licensePlateReservations.map((plateReservation) => (
-                        <div>
+                        <div key={plateReservation._id}>
                             <LicensePlate
+                                key={plateReservation._id}
                                 licensePlate={plateReservation.licensePlate}
                             />
                             {plateReservation.expiryDate}
@@ -74,7 +32,7 @@ export const LicensePlateReservationList = ({ licensePlateReservations }) => {
                     to={'/addLicensePlateReservation'}
                     aria-label="addLicensePlateReservation"
                 >
-                    <AddIcon />s
+                    <AddIcon />
                 </IconButton>
             </Card>
         </Grid>
--- frontend/src/components/VehicleListPaper.js
+++ frontend/src/components/VehicleListPaper.js
@@ -20,6 +20,13 @@ import LicensePlate from './LicensePlate';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import clsx from 'clsx';
 
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
 import LicensePlateService from '../services/LicensePlateService';
 
 const makeLogos = require('../../resources/carLogos');
@@ -30,7 +37,8 @@ const styles = (theme) => ({
         marginLeft: 'auto',
         transition: theme.transitions.create('transform', {
             duration: theme.transitions.duration.shortest
-        })
+        }),
+        willChange: 'transform, box-shadow, z-index'
     },
     expandOpen: {
         transform: 'rotate(180deg)'
@@ -134,8 +142,24 @@ class VehicleListPaper extends React.Component {
                     </CardActions>
                     <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
                         <CardContent>
-                            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                            minutes.
+                            <Table className={classes.table} aria-label="simple table">
+                                <TableHead>
+                                <TableRow>
+                                    <TableCell>Process type</TableCell>
+                                    <TableCell align="right">Last Update</TableCell>
+                                </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                {this.props.vehicle.processes.map((process) => (
+                                    <TableRow key={process._id}>
+                                    <TableCell component="th" scope="row">
+                                        {process.processType}
+                                    </TableCell>
+                                    <TableCell align="right">{new Date(Date.parse(process.date)).toUTCString()}</TableCell>
+                                    </TableRow>
+                                ))}
+                                </TableBody>
+                            </Table>
                         </CardContent>
                     </Collapse>
                 </Card>
