833bc83c34353b11dd58ae2aa75f88e54aec34cc
Janik Schnellbach <janik.schnellbach@gmail.com>
2021-07-25 18:17:49 +0200

    Some more text on license plate reservation

--- frontend/src/components/LicensePlateReservationList.js
+++ frontend/src/components/LicensePlateReservationList.js
@@ -189,7 +189,8 @@ class LicensePlateReservationList extends React.Component {
                     </TableBody>
                     <caption style={{ textAlign: 'center' }}>
                         You can use your license plates as part of your vehicle
-                        registration.
+                        registration. Expired license plate reservations are highlighted
+                        in red.
                     </caption>
                 </Table>
             </TableContainer>
